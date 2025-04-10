afbef2881a79756802e6a487bfc1b3a09f07a9b0
Matheus Marques <marquesrmatheus@gmail.com>
2025-04-10 11:02:09 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const map = L.map('map').setView([-15.7942, -47.8822], 4); // Brasil
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
-L.marker([-15.7942, -47.8822]).addTo(map)
-  .bindPopup('Brasília')
-  .openPopup();
